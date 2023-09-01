940ec2bf13bce6a9a542f7fd89c2ec941d950b8b
Eugene Molotov <forever_air@192.168.0.11>
2023-09-01 19:09:54 +0500

    improve UI

--- app/components/WeatherDetails.tsx
+++ app/components/WeatherDetails.tsx
@@ -10,11 +10,11 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
   return (
     <>
       <div className="p-12">
-        <h1 className="mb-4 text-2xl text-sky-200">
+        <h1 className="mb-4 text-2xl text-sky-200 border-b-2 border-b-sky-100 w-1/3">
           Weather Details
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Wind speed</h3>
               <h3 className="text-sky-900">{data?.current?.wind_kph} km/h</h3>
@@ -23,16 +23,16 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
               <GiWindSlap fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
-              <h3 className="text-sky-900">WiHumidity</h3>
+              <h3 className="text-sky-900">Humidity</h3>
               <h3 className="text-sky-900">{data?.current?.humidity}%</h3>
             </div>
             <div className="text-sky-900">
               <WiHumidity fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Wind direction</h3>
               <h3 className="text-sky-900">{data?.current?.wind_dir}</h3>
@@ -41,7 +41,7 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
               <GiCompass fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Sunrise</h3>
               <h3 className="text-sky-900">{data?.forecast?.forecastday[0].astro.sunrise}</h3>
@@ -50,7 +50,7 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
               <BsSunrise fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Sunset</h3>
               <h3 className="text-sky-900">{data?.forecast?.forecastday[0].astro.sunset}</h3>
@@ -59,7 +59,7 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
               <BsSunset fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Air Pressure</h3>
               <h3 className="text-sky-900">{data?.current?.pressure_mb} hPa</h3>
@@ -68,7 +68,7 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
               <MdAir fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Feels Like</h3>
               <h3 className="text-sky-900">{data?.current?.feelslike_c}°</h3>
@@ -77,7 +77,7 @@ const WeatherDetails = ({ data }: WeatherDetailsProps) => {
               <CiTempHigh fontSize={40} />
             </div>
           </div>
-          <div className="bg-sky-100/50 flex p-4 items-center justify-center gap-6 rounded-xl">
+          <div className="bg-sky-100/50 flex p-4 items-center justify-between gap-6 rounded-xl">
             <div className="text-2xl">
               <h3 className="text-sky-900">Visibility</h3>
               <h3 className="text-sky-900">{data?.current?.vis_km} km</h3>
--- app/components/WeekForecast.tsx
+++ app/components/WeekForecast.tsx
@@ -5,7 +5,7 @@ const WeekForecast = ({ data }: WeekForecastProps) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-8 w-full">
       {data?.forecast?.forecastday.map((day, index) => (
-        <div key={index} className="bg-gradient-to-b from-sky-700/60 to-sky-900/60 p-2 text-center rounded-lg flex flex-col items-center">
+        <div key={index} className="bg-gradient-to-b from-sky-700/60 to-sky-900/60 p-2 text-sky-200 text-center rounded-lg flex flex-col items-center hover:bg-sky-200/70 transition-colors duration-300 ease-in-out delay-50">
           <p>
             {getShortDate(day?.date || '')}
           </p>
